d2c2a4c1314683908c4896792f93051a052fcf78
Minita Dabhi <dabhi.minita@gmail.com>
2017-11-08 13:08:39 -0500

    Adding Calc.py

---
+++ Calc.py
@@ -0,0 +1,21 @@
+def addition(x, y):
+    """Add function"""
+    return x + y
+
+
+def subtract(x, y):
+    """Subtract function"""
+    return x - y
+
+
+def multiply(x, y):
+    """Multiply function"""
+    return x * y
+
+
+def divide(x, y):
+    """Division function"""
+    if y == 0:
+        raise ValueError("Can not divide by the zero!")
+    return x / y
+
